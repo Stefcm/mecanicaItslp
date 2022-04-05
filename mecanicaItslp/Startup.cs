24a0d11a16c2507acf07649fb1d6ced44a2d7b0a
Stefcm <scorderom88@gmail.com>
2022-04-04 21:48:48 -0500

    Agregar archivos de proyecto.

---
+++ mecanicaItslp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mecanicaItslp.Startup))]
+namespace mecanicaItslp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
